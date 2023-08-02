bb0b09f3e5ff1d0ac6cebd992403a56acb064a42
ranrii <lp.somsap@gmail.com>
2023-08-03 00:40:10 +0700

    update codes for postgresql

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ gravatar = Gravatar(
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -250,4 +250,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
